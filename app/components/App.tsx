ab288e6bc175c7646ad70074fe0789b977a74f23
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-08-29 22:54:28 +0800

    Initial React work

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -2,15 +2,12 @@ import '../assets/app.sass';
 import Game from './Game';
 import Rules from './Rules';
 import Multiplayer from './Multiplayer';
-import { useState } from 'react';
 import { Flex } from '@chakra-ui/react';
 
 
 const App = () => {
   // blue, cyan, gray, green, orange, pink, purple, red, teal, yellow
   const color = "teal";
-  const [page, setPage] = useState("game");
-
 
   return (
     <>
