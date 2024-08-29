698cd6cc5a6d03cf1b54b426996d851185a93591
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-08-29 21:30:36 +0800

    Skeleton page of game

---
+++ app/components/App.tsx
@@ -0,0 +1,26 @@
+import '../assets/app.sass';
+// import NavBar from './NavBar';
+// import Game from './Game';
+// import About from './About';
+// import Rules from './Rules';
+import { useState } from 'react';
+
+
+const App = () => {
+  // blue, cyan, gray, green, orange, pink, purple, red, teal, yellow
+  const color = "teal";
+  const [page, setPage] = useState("game");
+
+
+  return (
+    <>
+      {/* <NavBar color={color} updatePage={setPage} />
+
+      {page === "game" && <Game color={color} updatePage={setPage}/>}
+      {page === "about" && <About color={color} />}
+      {page === "rules" && <Rules color={color}/>} */}
+    </>
+  );
+}
+
+export default App;
