a7b4129cf12a7e2b46d89efb7997e2dbaa0d0b9e
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-08-31 10:02:52 +0800

    Added file tracing excludes

--- next.config.js
+++ next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
       },
     ]
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/@swc/core-linux-x64-gnu",
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/@esbuild/linux-x64",
+      ],
+    },
+  }
 }
 
 module.exports = nextConfig
